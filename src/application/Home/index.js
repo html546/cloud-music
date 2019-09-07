c04bdd1afa4d4a0097c90c20b12672e3f3451491
html546 <1776787598@qq.com>
2019-09-07 09:01:48 +0800

    首页部分样式

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config'
-import { Top } from './style'
+import { Top, Tab, TabItem } from './style'
+import { NavLink } from 'react-router-dom'
 
 const Home = (props) => {
     const { route } = props;
@@ -11,6 +12,23 @@ const Home = (props) => {
                 <span className="title">WebApp</span>
                 <span className="iconfont search">&#xe62b;</span>
             </Top>
+            <Tab>
+                <NavLink to="/recommend" activeClassName="selected">
+                    <TabItem>
+                        <span>推荐</span>
+                    </TabItem>
+                </NavLink>
+                <NavLink to="/singers" activeClassName="selected">
+                    <TabItem>
+                        <span>歌手</span>
+                    </TabItem>
+                </NavLink>
+                <NavLink to="/rank" activeClassName="selected">
+                    <TabItem>
+                        <span>排行榜</span>
+                    </TabItem>
+                </NavLink>
+            </Tab>
             {renderRoutes(route.routes)}
         </div>
     );
